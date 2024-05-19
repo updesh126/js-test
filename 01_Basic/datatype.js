f2e15558b6105f319554f49a57f5a213a6a2f24e
Updesh Pr. Singh <updesh071@gmail.com>
2024-05-19 15:10:02 +0530

    datatype, conversion

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newr version
+
+// alert(3+3) // we are using node js not brower
+
+console.log(3+3);
+ let name ="updesh"
+ let age =22
+ let isLoggedIn = false
+ // console.log(name,age,isLoggedIn)
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => empty value  standalone value
+ // undefined => 
+ // symbol => unique
+
+ // object
+
+ console.log(typeof null)
+ console.log(typeof undefined);
+
+ 
--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -1,1 +1,12 @@
 console.log("hello ji")
+
+const accountId=1444553
+let accountName="Rahul"
+let accountEmail ="updesh071@gmail.com"
+let accountPassword="123456789"
+accountCity="Jaipur"
+
+console.log(accountId);
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity])
+
